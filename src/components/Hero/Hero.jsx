897392faae654eada25d4c843735a3f7a621440f
nuriasky <nuriasky814@gmail.com>
2023-09-29 17:24:19 -0300

    hero finished

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
-import { HeroStyled, HeroText, HeroTextContainer, HeroTitle, TitleSpan } from './HeroStyles'
+import { HeroGetStarted, HeroImageStyled, HeroStyled, HeroText, HeroTextContainer, HeroTitle, TitleSpan } from './HeroStyles'
+
+import { FaArrowRight } from "react-icons/fa"
+
+import { color, motion } from "framer-motion"
 
 const Hero = () => {
   return (
     <HeroStyled>
-        <HeroTextContainer>
+        <HeroImageStyled>
+            <img
+            src='https://res.cloudinary.com/dbixsporz/image/upload/v1695939872/qfm6pxpfowai0vzx594m.png'
+            />
+        </HeroImageStyled>
 
+        <HeroTextContainer>
             <HeroTitle>disfruta de los <TitleSpan>mejores sabores!</TitleSpan></HeroTitle>
 
             <HeroText>Fiamma’s Bake ha perdurado en el tiempo combinando el arte de vivir, el buen comer y la creatividad.</HeroText>
 
+            <motion.div whileHover={{scale: 1.06}}>
+                <a href="/#">
+                    <HeroGetStarted>
+                        <p>Conoce más...</p>
+                        <FaArrowRight/>           
+                    </HeroGetStarted>
+                </a>
+            </motion.div>
+
+
         </HeroTextContainer>
+
     </HeroStyled>
   )
 }
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -2,10 +2,12 @@ import styled from "styled-components"
 
 export const HeroStyled = styled.section `
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
+    justify-content: center;
+    gap: 60px;
     max-width: 1300px;
-    height: 600px;
+    height: 720px;
 
     @media (max-width: 992px) {
         flex-direction: column;
@@ -20,10 +22,11 @@ export const HeroTextContainer = styled.div `
 `;
 
 export const HeroTitle = styled.h1 `
-    width: 400px;
+    margin: 5px;
+    width: 450px;
     text-align: center;
     color: #3D3534;
-    font-size: 35px;
+    font-size: 38px;
     font-family: 'bricolage grotesque';
     text-transform: uppercase;
 `;
@@ -37,8 +40,37 @@ export const TitleSpan = styled.span `
 `;
 
 export const HeroText = styled.p `
-    width: 320px;
+    width: 360px;
     color: #3D3534;
     font-weight: 500;
-    font-size: 18px;
+    font-size: 20px;
+`;
+
+export const HeroGetStarted = styled.div`
+    font-size: 28px;
+    color: var(--primary-pink);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    align-content: center;
+    gap: 10px;
+
+    p {
+        color: var(--dark-bg);
+        font-weight: 600;
+        transition: all 0.3 ease-in;
+    }
+    
+    p:hover {
+        color: #473736;
+        font-weight: 700;
+        transition: all 0.3 ease-out;
+    }
+`;
+
+//IMAGE
+export const HeroImageStyled = styled.div`
+    img {
+        width: 570px;
+    }
 `;
